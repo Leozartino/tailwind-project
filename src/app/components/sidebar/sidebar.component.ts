2b1889a2daf7dbd6c87a61019180ed8d38a0ee0e
Leonardo Oliveira <leozartino@gmail.com>
2024-03-31 14:13:27 +0200

    refact: update sidebar component with mainNavigation

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MainNavigationComponent } from './main-navigation/main-navigation.component';
 
 @Component({
   selector: 'project-sidebar',
   standalone: true,
-  imports: [],
+  imports: [MainNavigationComponent],
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {}
